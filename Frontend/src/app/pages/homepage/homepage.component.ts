4ed8bf90e4ae35c378a321c34a9f06e1d4539754
NiklasKoenig <mail@niklaskoenig.de>
2024-10-11 23:53:19 +0200

    added welcome page, header and social media buttons

---
+++ Frontend/src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, FontAwesomeModule, CommonModule],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.scss',
+})
+export class HomepageComponent {
+  //
+  public socials: any = [
+    {
+      icon: faInstagram,
+      url: 'https://www.instagram.com/nklsk23/',
+      name: 'Instagram',
+    },
+    {
+      icon: faTwitter,
+      url: 'https://x.com/dukedo7',
+      name: 'Twitter',
+    },
+    {
+      icon: faLinkedin,
+      url: 'https://www.linkedin.com/in/niklas-k%C3%B6nig-22b143322/',
+      name: 'LinkedIn',
+    },
+  ];
+}
